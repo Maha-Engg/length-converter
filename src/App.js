1d276336601a4bbae3778106152d61e858305d0d
Maha-Engg <mahacm98@gmail.com>
2023-05-14 19:03:21 +0530

    add length-converter master

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+const LengthConverter = () => {
+  const [fromValue, setFromValue] = useState('');
+  const [toValue, setToValue] = useState('');
+  const [fromUnit, setFromUnit] = useState('meters');
+  const [toUnit, setToUnit] = useState('feet');
 
-function App() {
+  const convertLength = () => {
+    // Define conversion factors for each unit
+    const conversionFactors = {
+      meters: {
+        feet: 3.28084,
+        inches: 39.3701,
+        centimeters: 100,
+      },
+      feet: {
+        meters: 0.3048,
+        inches: 12,
+        centimeters: 30.48,
+      },
+      inches: {
+        meters: 0.0254,
+        feet: 0.0833333,
+        centimeters: 2.54,
+      },
+      centimeters: {
+        meters: 0.01,
+        feet: 0.0328084,
+        inches: 0.393701,
+      },
+    };
+
+    // Convert from the "from" unit to the "to" unit
+    const factor = conversionFactors[fromUnit][toUnit];
+    const result = fromValue * factor;
+
+    // Set the result state
+    setToValue(result);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <h2>Length Converter</h2>
+      <div className='unit'>
+        <label>
+          From:
+          <input
+            type="number"
+            value={fromValue}
+            onChange={(e) => setFromValue(e.target.value)}
+          />
+          <select  className='select' value={fromUnit} onChange={(e) => setFromUnit(e.target.value)}>
+            <option value="meters">Meters</option>
+            <option value="feet">Feet</option>
+            <option value="inches">Inches</option>
+            <option value="centimeters">Centimeters</option>
+          </select>
+        </label>
+      </div>
+      <div className='to'>
+        <label>
+          To:
+          <input type="text" value={toValue} readOnly />
+          <select className='select' value={toUnit} onChange={(e) => setToUnit(e.target.value)}>
+            <option value="meters">Meters</option>
+            <option value="feet">Feet</option>
+            <option value="inches">Inches</option>
+            <option value="centimeters">Centimeters</option>
+          </select>
+        </label>
+      </div>
+      <button className='btn' onClick={convertLength}>Convert</button>
     </div>
   );
-}
+};
 
-export default App;
+export default LengthConverter;
